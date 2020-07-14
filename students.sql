e66d15dd1e47def0ec2f4911df1ab8d46ca691f2
adreankibe <42399996+adreankibe@users.noreply.github.com>
2020-07-14 01:35:10 -0700

    Add files via upload

---
+++ students.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 14, 2020 at 09:18 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `students`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students_table`
+--
+
+CREATE TABLE `students_table` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `students_table`
+--
+
+INSERT INTO `students_table` (`id`, `name`, `email`) VALUES
+(1, 'adrean', 'adrean@strathmore.edu'),
+(2, 'john', 'john@strathmore.edu'),
+(3, 'phyllis', 'phyllis@strathmore.edu'),
+(4, 'mary', 'mary@strathmore.edu'),
+(5, 'Stanley', 'stanley@strrathmore.edu'),
+(6, 'arnold2', 'arnold@strathmore.edu');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `students_table`
+--
+ALTER TABLE `students_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `students_table`
+--
+ALTER TABLE `students_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
